ae202e5734cb9133b2ae4151e7af889c71511665
aakritipoudel15 <105117852+aakritipoudel15@users.noreply.github.com>
2024-01-13 07:49:01 +0000

    Commit made from VS Code Azure Static Web Apps

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -24,7 +24,7 @@ const imageAltText = "purple and blue abstract background";
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I'm a Computer Engineering student studying at Kathmandu University. I am passionate about solving problems in new creative ways to drive innovation.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
@@ -32,11 +32,9 @@ const description =
  */
 const skillsList = [
   "Web design",
-  "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
-  "Graphic design",
+  "Machine Learning",
+  "Data Science",
+
 ];
 
 /**
@@ -45,7 +43,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  " I am a hardworking and dedicated person. I am passionate about my work and I know how to get the job done. I am a quick learner and I believe in learning from my mistakes for I strongly feel that this virtue will take me ahead in life and career";
 
 const About = () => {
   return (
--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -31,28 +31,22 @@ const imageAltText = "desktop with books and laptop";
  */
 const projectList = [
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
+    title: "Resell-It",
     description:
-      "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
-    url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
+      " A mobile application that allows users to sell their used items and buy used items from other users. It is built using Flutter and Firebase.",
+    url: "https://github.com/aakritip15/E_commerce",
   },
   {
-    title: "Web Development for Beginners",
+    title: "LetsChat",
     description:
-      "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
-    url: "https://github.com/microsoft/web-dev-for-beginners",
+      " A mobile application that allows users to chat with each other. It is built using Flutter and Firebase.",
+    url: "https://github.com/aakritip15/LetsChat",
   },
   {
-    title: "My Resume Site",
+    title: "Brain MRI Segementation using U-Net",
     description:
-      "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
-    url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
-  },
-  {
-    title: "GitHub Codespaces and github.dev",
-    description:
-      "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
-    url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
+      " A deep learning model that can be used to segment brain tumors from MRI images. It is built using Tensorflow.",
+    url: "https://github.com/aakritip15/Brain-MRI-Segmentation-",
   },
 ];
 
